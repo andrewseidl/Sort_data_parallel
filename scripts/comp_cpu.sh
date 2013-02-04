dfae7db8e2d2032a3ec9e3a40a709dff5d3f9d88
Andrew Seidl <git@andrewseidl.com>
2013-02-04 10:57:51 -0600

    Must link against pthread in this version of g++

--- scripts/comp_cpu.sh
+++ scripts/comp_cpu.sh
@@ -1,4 +1,4 @@
-clang++ -O3 -std=c++11 -stdlib=libc++ utils.cpp parallel_sort_CPU.cpp -o clang_test
+#clang++ -O3 -std=c++11 -stdlib=libc++ utils.cpp parallel_sort_CPU.cpp -o clang_test
 #g++ -O3 -std=c++0x utils.cpp parallel_sort_CPU.cpp -o gcc_463_test
-g++ -O3 -std=c++11 utils.cpp parallel_sort_CPU.cpp -o gcc_472_test
+g++ -O3 -std=c++11 utils.cpp parallel_sort_CPU.cpp -lpthread -o gcc_472_test
 
