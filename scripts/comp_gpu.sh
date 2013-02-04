7cc5ee1bda6fa44d18699b1013b8e36f0e2d938f
Andrew Seidl <git@andrewseidl.com>
2013-02-04 11:28:09 -0600

    Compiling for compute capability 3.5 (Tesla K20)

--- scripts/comp_gpu.sh
+++ scripts/comp_gpu.sh
@@ -1,3 +1,3 @@
-g++ -c -O3 -std=c++0x utils.cpp
-nvcc -O3 -arch=sm_21 utils.o parallel_sort_GPU.cu -o nvcc_test
+g++ -c -O3 -std=c++11 utils.cpp
+nvcc -O3 -arch=sm_35 utils.o parallel_sort_GPU.cu -o nvcc_test
 
